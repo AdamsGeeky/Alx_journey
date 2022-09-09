ab54a1be8eccac33dc123cb0bbba9f5a0490fb69
AdamsGeeky <muhdadamu0008@gmail.com>
2022-09-10 00:09:46 +0100

    the fun part begin nw

--- blogs/src/pages/index.js
+++ blogs/src/pages/index.js
@@ -13,18 +13,18 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle"><Typewriter 
+           options={{
+             strings: ['Welcome to Geeks ink treasure','this is where i have been complaining my','Software Engineering journey'],
+             autoStart: true,
+             loop: true,
+           }}
+           /></p>
         <div className={styles.buttons}>
         <Link
            className="button button--secondary button--lg"
             to="/blog/welcome">
-          <Typewriter 
-           options={{
-             strings: ['✍ Software Engineering journey', '✍ Who Is An Engineer'],
-             autoStart: true,
-             loop: true,
-           }}
-           />
+          ✍ Who Is An Engineer ✍ 
          </Link>
         </div>
       </div>
