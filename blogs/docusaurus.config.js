c19e9b0e6d21d917f453b35f253e1dbfa213aab0
AdamsGeeky <muhdadamu0008@gmail.com>
2022-09-09 23:55:30 +0100

    type

--- blogs/docusaurus.config.js
+++ blogs/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Geeks Ink...',
-  tagline: 'Software Engineering journey',
+  tagline: '',
   url: 'https://adamsgeeky.netlify.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
--- blogs/src/pages/index.js
+++ blogs/src/pages/index.js
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import Typewriter from 'typewriter-effect';
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -15,11 +15,17 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
+        <Link
+           className="button button--secondary button--lg"
             to="/blog/welcome">
-            Who Is An Engineer ✍
-          </Link>
+          <Typewriter 
+           options={{
+             strings: ['✍ Software Engineering journey', '✍ Who Is An Engineer'],
+             autoStart: true,
+             loop: true,
+           }}
+           />
+         </Link>
         </div>
       </div>
     </header>
